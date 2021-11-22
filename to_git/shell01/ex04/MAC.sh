db4b80550df6b27f583f832b58c73cbe20c89917
Tamsi BESSON <tbesson@e3r12p15.42.fr>
2021-11-22 17:45:07 +0100

    piscine 42

--- to_git/shell01/ex04/MAC.sh
+++
@@ -1,1 +0,0 @@
-ifconfig | grep ether | tr -d '\t' | cut -c 6- | tr -d ' '
--- to_git/shell01/ex07/r_dwssap.sh
+++
@@ -1,1 +0,0 @@
-cat /etc/passwd | sed '/^#/d' | cut -f1 -d ':' | sed -n 'n;p' | rev | sort -rd | sed -n $FT_LINE1','$FT_LINE2'p' | tr '\n' ' ' | sed "s/ /, /g" | sed "s/, $/./g" | tr -d '\n'
